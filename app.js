0668184276bc7f36be227d18e938e631c31014b0
li-ming-hui <358166192@qq.com>
2018-03-25 21:40:33 +0800

    提取路由模块

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-// 0.加载express
+// 0.加载express router
 const express=require('express');
-// 1.调用express()得到一个app
+const router =require('./router');
+
 const app=express();
-// 2.设置请求对应的处理函数
-app.get('/',(rep,res)=>{
-  res.send('hello world')
-})
+
+app.use(router);
 // 3.监听端口号，启动服务器
 app.listen(3000,()=>console.log('running...'));
