f5588e31cf19e67189dac420b8ac0cc05ca7d875
li-ming-hui <358166192@qq.com>
2018-03-26 18:10:45 +0800

    完成基本的用户注册功能

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 // 0.加载express router
 const express=require('express');
+const bodyParser = require('body-parser');
 const router =require('./router');
 
 const app=express();
 
+// 1.配置模版引擎
+// 2.渲染页面
+// 3.开放静态资源
+// 4.下载第三方包
+// bootstrap@3.3.7   jquery
+app.use('/public',express.static('./public/'));
+app.use('/node_modules',express.static('./node_modules/'));
+
+// 配置 body-parser 解析表单 POST 请求体
+// 只有配置了该插件，就可以在请求处理函数中使用 req.body 来访问请求体数据了
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// 配置使用 art-template 模版引擎
+app.engine('html',require('express-art-template'));
 app.use(router);
 // 3.监听端口号，启动服务器
 app.listen(3000,()=>console.log('running...'));
